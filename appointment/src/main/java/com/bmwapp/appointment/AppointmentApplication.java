a210c6a19e7eb3a3103212bfe219d6587c0f72f0
Mateusz <marchewka.matthew@gmail.com>
2022-12-05 22:06:36 +0100

    setup eureka client for every microservice

--- appointment/src/main/java/com/bmwapp/appointment/AppointmentApplication.java
+++ appointment/src/main/java/com/bmwapp/appointment/AppointmentApplication.java
@@ -2,8 +2,10 @@ package com.bmwapp.appointment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AppointmentApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppointmentApplication.class, args);
--- flat/src/main/java/com/bmwapp/flat/FlatApplication.java
+++ flat/src/main/java/com/bmwapp/flat/FlatApplication.java
@@ -2,8 +2,10 @@ package com.bmwapp.flat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FlatApplication {
     public static void main(String[] args) {
         SpringApplication.run(FlatApplication.class, args);
