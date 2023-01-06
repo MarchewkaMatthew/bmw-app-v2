cfb18909a9dd75886b257c93a563d8064e9baea5
Mateusz <marchewka.matthew@gmail.com>
2023-01-06 15:17:02 +0100

    ..

--- message/src/main/java/com/bmwapp/message/config/MessageConfiguration.java
+++ message/src/main/java/com/bmwapp/message/config/MessageConfiguration.java
@@ -5,6 +5,9 @@ import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class MessageConfiguration {
@@ -13,5 +16,16 @@ public class MessageConfiguration {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
         return modelMapper;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
     }
 }
