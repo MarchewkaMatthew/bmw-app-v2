28e03f99d384370be495048cadbc4f48984addc6
Mateusz <marchewka.matthew@gmail.com>
2023-01-06 15:53:22 +0100

    finish cors

--- message/src/main/java/com/bmwapp/message/config/MessageConfiguration.java
+++ message/src/main/java/com/bmwapp/message/config/MessageConfiguration.java
@@ -5,9 +5,6 @@ import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class MessageConfiguration {
@@ -16,16 +13,5 @@ public class MessageConfiguration {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
         return modelMapper;
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-            }
-        };
     }
 }
