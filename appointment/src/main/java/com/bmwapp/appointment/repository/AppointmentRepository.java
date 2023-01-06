a1bf2b25d587245098fb20e36111e5f9f0b27a0c
patrykm5341 <patrykm5341@gmail.com>
2023-01-06 15:49:24 +0100

    Appointment Service and Repository done.

--- appointment/src/main/java/com/bmwapp/appointment/repository/AppointmentRepository.java
+++ appointment/src/main/java/com/bmwapp/appointment/repository/AppointmentRepository.java
@@ -3,5 +3,9 @@ package com.bmwapp.appointment.repository;
 import com.bmwapp.appointment.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
+
+    List<Appointment> getAllByCustomerId(String customerId);
 }
