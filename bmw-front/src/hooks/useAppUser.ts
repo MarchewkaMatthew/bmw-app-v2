9b5734ea2d542a33a59c2c409603350aff9e8a57
patrykm5341 <patrykm5341@gmail.com>
2023-01-07 09:07:06 +0100

    .

--- bmw-front/src/hooks/useAppUser.ts
+++ bmw-front/src/hooks/useAppUser.ts
@@ -1,26 +1,24 @@
-import { useKeycloak } from "@react-keycloak/web"
+import { useKeycloak } from "@react-keycloak/web";
 
 export type AppUser =
-  { _type: "UNINITIALIZED" }  |
-  { _type: "NOT_AUTHENTICATED", login: () => void, register: () => void} |
-  { _type: "AUTHENTICATED", token: string, userName: string, logout: () => void, isAgent: boolean; isCustomer: boolean }
-
+  | { _type: "UNINITIALIZED" }
+  | { _type: "NOT_AUTHENTICATED"; login: () => void; register: () => void }
+  | { _type: "AUTHENTICATED"; token: string; userName: string; logout: () => void; isAgent: boolean; isCustomer: boolean };
 
 export const useAppUser: () => AppUser = () => {
-  const { keycloak, initialized } = useKeycloak()
+  const { keycloak, initialized } = useKeycloak();
 
-  if(!initialized) {
-    return { _type: "UNINITIALIZED" }
+  if (!initialized) {
+    return { _type: "UNINITIALIZED" };
   }
 
   const { authenticated, login, register, token, tokenParsed, logout } = keycloak;
 
-  if(!authenticated) {
-    return { _type: "NOT_AUTHENTICATED", login, register }
+  if (!authenticated) {
+    return { _type: "NOT_AUTHENTICATED", login, register };
   }
-
-  const isAgent = keycloak.hasRealmRole("AGENT");
-  const isCustomer = keycloak.hasRealmRole("CUSTOMER")
+  const isAgent = keycloak.tokenParsed?.roles.includes("AGENT");
+  const isCustomer = keycloak.tokenParsed?.roles.includes("CUSTOMER");
 
   return { _type: "AUTHENTICATED", token: token || "ERROR", userName: tokenParsed?.preferred_username || "ERROR", logout, isAgent, isCustomer };
-}
+};
